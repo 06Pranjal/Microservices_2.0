c495704989ee7c870dffe5c872147a0de324c961
Pranjal Singh <155311442+06Pranjal@users.noreply.github.com>
2026-01-07 19:38:12 +0530

    Fixed errors completely now no issues

--- productService/src/test/java/com/microservice_project/productService/ProductServiceApplicationTests.java
+++ productService/src/test/java/com/microservice_project/productService/ProductServiceApplicationTests.java
@@ -1,23 +1,22 @@
 package com.microservice_project.productService;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.microservice_project.productService.dto.ProductRequest;
 import com.microservice_project.productService.repository.ProductRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.MediaType;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.containers.MongoDBContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.mongodb.MongoDBContainer;
-import tools.jackson.databind.ObjectMapper;
-
 
 import java.math.BigDecimal;
 
