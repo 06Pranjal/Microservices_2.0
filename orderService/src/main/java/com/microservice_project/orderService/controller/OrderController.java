91b8ce9dc41fd83db85330e12f5e3451845bb006
Pranjal Singh <155311442+06Pranjal@users.noreply.github.com>
2026-05-19 01:47:09 +0530

    After Adding a successfull circuit breaker now i have implemented timeout if service is taking time it will give response or go to open state,, after this retry functionalities will be implemented

--- orderService/src/main/java/com/microservice_project/orderService/controller/OrderController.java
+++ orderService/src/main/java/com/microservice_project/orderService/controller/OrderController.java
@@ -3,9 +3,12 @@ package com.microservice_project.orderService.controller;
 import com.microservice_project.orderService.dto.OrderRequest;
 import com.microservice_project.orderService.service.OrderService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api/order")
@@ -17,11 +20,12 @@ public class OrderController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     @CircuitBreaker(name="inventory", fallbackMethod = "fallbackMethod")
-    public String placeOrder(@RequestBody OrderRequest orderRequest){
-        orderService.placeOrder(orderRequest);
-        return "Order Placed Successfully";
+    @TimeLimiter(name="inventory")
+    public CompletableFuture<String> placeOrder(@RequestBody OrderRequest orderRequest){
+        return CompletableFuture.supplyAsync(()->orderService.placeOrder(orderRequest));
+
     }
-    public String fallbackMethod(OrderRequest orderRequest,RuntimeException runtimeException){
-        return "Oops! Something Went Wrong ..Please try after some time....!";
+    public CompletableFuture<String > fallbackMethod(OrderRequest orderRequest,Exception exception){
+        return CompletableFuture.supplyAsync(()->"Oops! Something Went Wrong ..Please try after some time....!");
     }
 }
--- orderService/src/main/java/com/microservice_project/orderService/service/OrderService.java
+++ orderService/src/main/java/com/microservice_project/orderService/service/OrderService.java
@@ -24,7 +24,7 @@ public class OrderService {
 
     private final WebClient.Builder webClientBuilder;
 
-    public void placeOrder(OrderRequest orderRequest){
+    public String placeOrder(OrderRequest orderRequest){
         Order order=new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
 
@@ -54,6 +54,7 @@ public class OrderService {
 
         if(allProductsInStock){
             orderRepository.save(order);
+            return "Order Placed Succesfully";
         }else{
             throw new IllegalArgumentException("Product is not in stock,please try again later");
         }
